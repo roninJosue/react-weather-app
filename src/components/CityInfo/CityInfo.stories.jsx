0517536b1e2f7457a3ff9d288e8c99046121c8da
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2021-12-20 22:17:33 -0600

    Initial Commit

---
+++ src/components/CityInfo/CityInfo.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react"
+import CityInfo from "./CityInfo";
+
+export default {
+  title: 'CityInfo',
+  component: CityInfo
+}
+
+export const CityExample = () => (<CityInfo city='León' country='Nicaragua' />)
