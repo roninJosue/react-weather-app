378c51b06a2b11970736024992af4b9a5394afba
Reynaldo Josué Cano Bárcenas <roninjosue88@gmail.com>
2022-01-09 20:17:32 -0600

    useCallback in the reducer.

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,38 @@
-import React, {useReducer} from 'react';
+import React, {useCallback, useReducer} from 'react';
 import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import MainPage from "./pages/MainPage";
 import CityPage from "./pages/CityPage";
 import NotFoundPage from "./pages/NotFoundPage";
 
+const initialValue = {
+  allWeather: {},
+  allChartData: {},
+  allForecastItemList: {}
+}
+
 const App = () => {
-  const initialValue = {
-    allWeather: {},
-    allChartData: {},
-    allForecastItemList: {}
-  }
-  const reducer = (state, action) => {
-    switch (action.type) {
-      case 'SET_ALL_WEATHER':
-        const weatherCity = action.payload
-        const newAllWeather =  {...state.allWeather, ...weatherCity}
-        return {...state, allWeather: newAllWeather}
-      case 'SET_CHART_DATA':
-        const chartData = action.payload
-        const newChartData = {...state.allChartData, ...chartData}
-        return {...state, allChartData: newChartData}
-      case 'SET_FORECAST_ITEM_LIST':
-        const forecast = action.payload
-        const newForecast = {...state.allForecastItemList, ...forecast}
-        return {...state, allForecastItemList: newForecast}
-      default:
-        return state
-    }
-  }
+  const reducer = useCallback(
+    (state, action) => {
+      switch (action.type) {
+        case 'SET_ALL_WEATHER':
+          const weatherCity = action.payload
+          const newAllWeather = {...state.allWeather, ...weatherCity}
+          return {...state, allWeather: newAllWeather}
+        case 'SET_CHART_DATA':
+          const chartData = action.payload
+          const newChartData = {...state.allChartData, ...chartData}
+          return {...state, allChartData: newChartData}
+        case 'SET_FORECAST_ITEM_LIST':
+          const forecast = action.payload
+          const newForecast = {...state.allForecastItemList, ...forecast}
+          return {...state, allForecastItemList: newForecast}
+        default:
+          return state
+      }
+    },
+    [],
+  );
 
   const [weatherState, dispatch] = useReducer(reducer, initialValue);
 
